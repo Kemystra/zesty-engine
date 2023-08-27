e2e85faaa2d569a6e351c79274b07a1825d85bf4
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-08-27 19:14:08 +0800

    Added unit test for Vector3D math
    Unit tests for Add, Sub, and Mul. Added Debug trait and also PartialEq trait for comparison usage. Added function new() as a generic numerical constructor for Vector3D.

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -1,10 +1,21 @@
 use std::ops::{Add, Sub, Mul};
 
 
+#[derive(Debug, PartialEq)]
 pub struct Vector3D {
     x: f64,
     y: f64,
     z: f64,
+}
+
+impl Vector3D {
+    pub fn new<T: Into<f64>>(x: T, y: T, z: T) -> Self {
+        Self {
+            x: x.into(),
+            y: y.into(),
+            z: z.into()
+        }
+    }
 }
 
 
@@ -39,5 +50,35 @@ impl Mul for Vector3D {
 
     fn mul(self, rhs: Self) -> Self::Output {
         self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_vector3d() {
+        let a = Vector3D::new(2,5,10);
+        let b = Vector3D::new(5,6,11);
+
+        assert_eq!(a+b, Vector3D::new(7,11,21));
+    }
+
+    #[test]
+    fn substract_vector3d() {
+        let a = Vector3D::new(4, 20, 5);
+        let b = Vector3D::new(8, 11, 7);
+
+        assert_eq!(a-b, Vector3D::new(-4, 9, -2));
+    }
+
+    #[test]
+    fn dot_product_vector3d() {
+        let a = Vector3D::new(5,6,7);
+        let b = Vector3D::new(10,2,3);
+
+        assert_eq!(a*b, 83_f64);
     }
 }
