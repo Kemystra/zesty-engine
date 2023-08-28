ba2ce178da675260a480166e297666df506b9cd9
Izzminhal Akmal bin Norhisyam <izzmin97@gmail.com>
2023-08-28 08:53:03 +0800

    Made Vector3D fields public
    Easier for direct access

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -3,9 +3,9 @@ use std::ops::{Add, Sub, Mul};
 
 #[derive(Debug, PartialEq)]
 pub struct Vector3D {
-    x: f64,
-    y: f64,
-    z: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
 impl Vector3D {
