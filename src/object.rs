740be863e56dd10d7f04b81f00d05dfc4a6e9e00
Izzminhal Akmal bin Norhisyam <izzmin97@gmail.com>
2023-08-28 10:42:29 +0800

    Made Object struct public
    Its Transform member is also made public for access later

--- src/object.rs
+++ src/object.rs
@@ -2,7 +2,7 @@ use crate::transform::Transform;
 use crate::math_utils::Vector3D;
 
 
-struct Object {
-    transform: Transform,
+pub struct Object {
+    pub transform: Transform,
     triangles: Vec<Vector3D> 
 }
