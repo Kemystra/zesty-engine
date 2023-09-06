67cc1f43960d97c721a50bef02b4ffe712ccb71d
Izzminhal Akmal bin Norhisyam <izzmin97@gmail.com>
2023-09-06 15:44:23 +0800

    Readded RenderMode enum into scene.rs
    Final refactoring... for now

--- src/scene.rs
+++ src/scene.rs
@@ -3,8 +3,17 @@ use crate::object::Object3D;
 
 #[derive(Debug)]
 pub struct Scene {
-    pub objects: Vec<Object3D>
+    pub objects: Vec<Object3D>,
+    pub render_mode: RenderMode
 }
+
+#[derive(Debug)]
+pub enum RenderMode {
+    VertexOnly,
+    Wireframe,
+    Full
+}
+
 
 impl Scene {
     pub fn render(&mut self, buffer: &mut [u8], pitch: usize) -> () {
