1d44075f38e95116660583e5e3a30a391bb44c86
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-09-10 15:15:49 +0800

    Brought Matrix3x4 mathematics to math_utils.rs
    Just to collect them together

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -54,6 +54,88 @@ impl Mul for Vector3D {
 }
 
 
+pub type Matrix3x4 = [[f64; 3]; 4];
+
+pub const NIL_MATRIX3X4: Matrix3x4 = [
+    [1.0, 0.0, 0.0], 
+    [0.0, 1.0, 0.0], 
+    [0.0, 0.0, 1.0], 
+    [0.0, 0.0, 0.0]
+];
+
+pub fn invert_matrix(matrix: &Matrix3x4) -> Result<Matrix3x4, String> {
+    const ROW: usize = 4;
+    const COL: usize = 3;
+
+    let mut matrix = matrix.clone();
+    let mut inv_matrix = NIL_MATRIX3X4.clone();
+
+    for column in 0..COL {
+        // Making sure pivot is a non-zero number
+        // If zero, swap row with one that has the biggest absolute value
+        let mut pivot = column;
+        let mut pivot_val = matrix[column][column];
+
+        if pivot_val == 0.0 {
+            for curr_row in 0..ROW {
+                if matrix[curr_row][column].abs() > pivot_val.abs() {
+                    pivot = curr_row;
+                    pivot_val = matrix[curr_row][column];
+                }
+            }
+
+            if pivot_val == 0.0 { return Err("Matrix has no inverse".to_string()) }
+            let mut tmp = matrix[pivot];
+            matrix[pivot] = matrix[column];
+            matrix[column] = tmp;
+
+            tmp = inv_matrix[pivot];
+            inv_matrix[pivot] = inv_matrix[column];
+            inv_matrix[column] = tmp;
+
+
+        }
+
+        // Forward substitution
+        for row_under_pivot in (column + 1)..ROW {
+            // Refer to scratchapixel.com, under Gauss-Jordan Matrix Inverse
+            // cuz I dunno wth I'm doing
+            let multiplier = matrix[row_under_pivot][column] / pivot_val;
+            for i in 0..COL {
+                matrix[row_under_pivot][i] -= multiplier * matrix[column][i];
+                inv_matrix[row_under_pivot][i] -= multiplier * inv_matrix[column][i];
+            }
+
+            matrix[row_under_pivot][column] = 0.0;
+        }
+    }
+
+    // Divide each row to turn the pivot into 1
+    for column in 0..COL {
+        let divisor = matrix[column][column];
+        for i in 0..COL {
+            matrix[column][i] /= divisor;
+            inv_matrix[column][i] /= divisor;
+        }
+        matrix[column][column] = 1.0;
+    }
+
+    // Backward substitution
+    for row in 0..ROW {
+        for column in (row+1)..COL {
+            let constant = matrix[row][column];
+            for i in 0..COL {
+                matrix[row][i] -= matrix[column][i] * constant;
+                inv_matrix[row][i] -= inv_matrix[column][i] * constant;
+            }
+
+            matrix[row][column] = 0.0;
+        }
+    }
+    Ok(inv_matrix)
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
