ce9ee3690c37201fd87c3df35f0ffa93e4d26487
Izzminhal Akmal bin Norhisyam <izzmin97@gmail.com>
2023-09-10 10:36:41 +0800

    Updated scene.rs to use latest Transform API

--- src/scene.rs
+++ src/scene.rs
@@ -1,5 +1,4 @@
 use crate::object::{Object3D, Camera};
-use crate::transform::convert_space;
 
 #[derive(Debug)]
 pub struct Scene {
@@ -26,11 +25,10 @@ impl Scene {
     }
 
     fn vertex_render(&mut self, buffer: &mut [u8], pitch: usize) -> () {
-        let world_to_cam_transform = self.camera.transform.invert_matrix().unwrap();
         for obj in self.objects.iter() {
             for vertex in obj.get_vertices() {
-                let vertex_in_world = convert_space(&obj.transform, &vertex);
-                let vertex_in_cam = convert_space(&world_to_cam_transform, &vertex_in_world);
+                let vertex_in_world = obj.transform.to_world_space(&vertex);
+                let vertex_in_cam = self.camera.transform.to_local_space(&vertex_in_world);
             }
         }
     }
