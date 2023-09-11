d2c6e215e54fd7027a23ca7a6a6175a8bef3a04f
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-09-11 11:13:06 +0800

    Added project_to_screen_space()
    Converting the points to screen space

--- src/object.rs
+++ src/object.rs
@@ -46,5 +46,32 @@ impl Camera {
             projection_data: ProjectionData::generate(n.into(), f.into(), fov.into()),
             dirty_flag: false
         }
-    } 
+    }
+
+    pub fn get_projection_data(&mut self) -> &ProjectionData {
+        if self.dirty_flag {
+            self.projection_data = ProjectionData::generate(
+                self.near_clip_distance, 
+                self.far_clip_distance,
+                self.field_of_view
+            )
+        }
+        &self.projection_data
+    }
+
+    pub fn project_to_screen_space(&mut self, point: Vector3D) -> Vector3D {
+        // Deconstructing the data
+        // What the actual frick
+        let ProjectionData(w_scaler, h_scaler, m1, m2) = self.get_projection_data();
+        let x = point.x * w_scaler;
+        let y = point.y * h_scaler;
+        let z = (point.z * m1) + m2;
+        let w = -point.z;
+
+        Vector3D {
+            x: x/w,
+            y: y/w,
+            z: z/w,
+        }
+    }
 }
