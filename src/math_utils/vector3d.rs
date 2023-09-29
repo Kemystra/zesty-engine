71e2d83f0a1a30a801b4d7d226485e76ab4d424d
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-09-29 15:57:52 +0800

    Placed Vector3D code into its file

---
+++ src/math_utils/vector3d.rs
@@ -0,0 +1,53 @@
+use std::ops::{Add, Sub, Mul};
+
+#[derive(Debug, PartialEq)]
+pub struct Vector3D {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vector3D {
+    pub fn new<T: Into<f64>>(x: T, y: T, z: T) -> Self {
+        Self {
+            x: x.into(),
+            y: y.into(),
+            z: z.into()
+        }
+    }
+}
+
+
+impl Add for Vector3D {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z
+        }
+    }
+}
+
+
+impl Sub for Vector3D {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z
+        }
+    }
+}
+
+// Dot product for vector
+impl Mul for Vector3D {
+    type Output = f64;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+}
