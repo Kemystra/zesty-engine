c3efa751123b50f955c37645c4f98ed449d00665
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-09-30 12:20:26 +0800

    Put raster-space coordinate into a temporary Vec

--- src/scene.rs
+++ src/scene.rs
@@ -28,18 +28,12 @@ impl Scene {
     pub fn render(&mut self, buffer: &mut [u32]) -> () {
         let rot = (PI/4.0) * (1.0/60.0);
         for obj in self.objects.iter_mut() {
+
+            let mut tmp_vertex: Vec<Vector3D> = vec![];
             for vertex in obj.get_vertices() {
                 let vertex_in_world = obj.transform.to_world_space(*vertex);
                 let vertex_in_cam = self.camera.transform.to_local_space(vertex_in_world);
                 let screen_coords = self.camera.project_to_screen_space(vertex_in_cam);
-
-                if screen_coords.x > 1.0 || screen_coords.x < -1.0 {
-                    continue;
-                }
-
-                if screen_coords.y > 1.0 || screen_coords.y < -1.0 {
-                    continue;
-                }
 
                 let ncd_coords = Vector3D {
                     x: (screen_coords.x + 1.0)/2.0,
@@ -47,20 +41,12 @@ impl Scene {
                     z: (screen_coords.z + 1.0)/2.0,
                 };
 
-                let final_x = (ncd_coords.x * SCREEN_WIDTH as f64) as usize; 
-                let final_y = (ncd_coords.y * SCREEN_HEIGHT as f64) as usize;
-
-
-                for i in 0..5 {
-                    for j in 0..5 {
-                        color_pixel(
-                        clamp(final_x + i, 0, SCREEN_WIDTH-1),
-                        clamp(final_y + j, 0, SCREEN_HEIGHT-1),
-                        WHITE,
-                        buffer);
-                    }
-                }
+                let final_x = ncd_coords.x * SCREEN_WIDTH as f64;
+                let final_y = ncd_coords.y * SCREEN_HEIGHT as f64;
+                tmp_vertex.push(Vector3D::new(final_x, final_y, screen_coords.z));
             }
+
+
             obj.transform.rotate(rot, 0.0, rot);
         }
     }
