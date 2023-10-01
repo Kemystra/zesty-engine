5a2837dc72f87ef842f73a5d5847584f443ac744
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-10-01 23:19:40 +0800

    Added initial try of implementing bresenham line

--- src/graphic.rs
+++ src/graphic.rs
@@ -1,7 +1,7 @@
 use crate::SCREEN_WIDTH;
 
 
-pub fn color_pixel(
+pub fn plot_pixel(
     x: usize, y: usize,
     color: (u8, u8, u8),
     buffer: &mut [u32]) -> () {
@@ -11,4 +11,20 @@ pub fn color_pixel(
     let offset = x + (y*SCREEN_WIDTH);
 
     buffer[offset] = color_32bit;
+}
+
+pub fn bresenham_line(x0: usize, y0: usize, x1: usize, y1: usize) {
+    let curr_x = x0 as isize;
+    let curr_y = y0 as isize;
+
+    let dx = ((x1 - x0) as isize).abs();
+    let dy = -((y1 - y0) as isize).abs();
+    let error = dx + dy;
+
+    let sx = if x0 < x1 {1} else {-1};
+    let sy = if y0 < y1 {1} else {-1};
+
+    loop {
+        plot_pixel()
+    }
 }
