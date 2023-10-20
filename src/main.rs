cceff3decef9f22d84c462c2e8e396f159be34f3
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-10-20 12:38:15 +0800

    Minor warning silenced

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ pub fn main() {
 
     // Boilerplate section for testing
     let mut cube = Object3D::load_obj("test_scene/tinker.obj".to_string()).unwrap();
-    cube.transform.translate(Vector3D::new(0, 0, 5));
+    cube.transform.translate(Vector3D::new(0, 0, 80));
 
     let camera = Camera::new(1, 30, 90, AspectRatio(16.0, 9.0));
 
@@ -62,7 +62,7 @@ pub fn main() {
             Event::WindowEvent { window_id, event: WindowEvent::CloseRequested }
                 if window_id == window.id() => { *control_flow = ControlFlow::Exit }
 
-            Event::WindowEvent { window_id, event: WindowEvent::KeyboardInput { input: input, ..} }
+            Event::WindowEvent { window_id, event: WindowEvent::KeyboardInput { input, ..} }
                 if window_id == window.id() => {
                     if input.virtual_keycode == Some(VirtualKeyCode::Space) && input.state == ElementState::Pressed {
                         println!("FPS: {}", 1_000_000 / delta_time);
