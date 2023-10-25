7c52eb1eb39bb25bf9de94df86c2985b06e5f002
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-10-25 14:36:42 +0800

    Added Component trait and ComponentType enum for Component system

--- src/component.rs
+++ src/component.rs
@@ -1,1 +1,41 @@
+use core::fmt;
 
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum ComponentType {
+    Mesh
+}
+
+pub trait Component {
+    fn get_type(&self) -> ComponentType;
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestComponent {
+        component_type: ComponentType
+    }
+
+    impl TestComponent {
+        fn new() -> Self {
+            Self {
+                component_type: ComponentType::Mesh
+            }
+        }
+    }
+
+    impl Component for TestComponent {
+        fn get_type(&self) -> ComponentType {
+            self.component_type
+        }
+    }
+
+    #[test]
+    fn get_type() {
+        let test_comp = TestComponent::new();
+        assert_eq!(test_comp.get_type(), ComponentType::Mesh);
+    }
+}
