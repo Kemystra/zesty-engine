5d23ece85189b58d2de5d8cb7d5e885b2a2e6dde
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-10-27 11:52:36 +0800

    Added Component and ComponentType trait to Mesh

--- src/component/mesh.rs
+++ src/component/mesh.rs
@@ -1,7 +1,8 @@
 use crate::math_utils::vector3d::Vector3D;
+use super::{Component, ComponentType};
 
 
-#[derive(Debug, )]
+#[derive(Debug, Component, ComponentType)]
 pub struct Mesh {
     src: String,
     vertices: Vec<Vector3D>,
