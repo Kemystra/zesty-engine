7f7a37423271ed9cfa975891dc1b9751450d0001
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-11-03 22:32:19 +0800

    Added homogenous option to invert_matrix() and updated the test

--- src/math_utils/matrix4x4.rs
+++ src/math_utils/matrix4x4.rs
@@ -7,21 +7,20 @@ pub const NIL_MATRIX4X4: Matrix4x4 = [
     [0.0, 0.0, 0.0, 0.0]
 ];
 
-pub fn invert_matrix(matrix: &Matrix4x4, ignore_4th_col: bool) -> Result<Matrix4x4, String> {
-    let ROW: usize = 4;
-    let COL: usize = if ignore_4th_col { 3 } else { 4 };
+pub fn invert_matrix(matrix: &mut Matrix4x4, ignore_4th_col: bool) -> Result<(), String> {
+    let row: usize = 4;
+    let col: usize = if ignore_4th_col { 3 } else { 4 };
 
-    let mut matrix = matrix.clone();
     let mut inv_matrix = NIL_MATRIX4X4.clone();
 
-    for column in 0..COL {
+    for column in 0..col {
         // Making sure pivot is a non-zero number
         // If zero, swap row with one that has the biggest absolute value
         let mut pivot = column;
         let mut pivot_val = matrix[column][column];
 
         if pivot_val == 0.0 {
-            for curr_row in 0..ROW {
+            for curr_row in 0..row {
                 if matrix[curr_row][column].abs() > pivot_val.abs() {
                     pivot = curr_row;
                     pivot_val = matrix[curr_row][column];
@@ -36,16 +35,14 @@ pub fn invert_matrix(matrix: &Matrix4x4, ignore_4th_col: bool) -> Result<Matrix4
             tmp = inv_matrix[pivot];
             inv_matrix[pivot] = inv_matrix[column];
             inv_matrix[column] = tmp;
-
-
         }
 
         // Forward substitution
-        for row_under_pivot in (column + 1)..ROW {
+        for row_under_pivot in (column + 1)..row {
             // Refer to scratchapixel.com, under Gauss-Jordan Matrix Inverse
             // cuz I dunno wth I'm doing
             let multiplier = matrix[row_under_pivot][column] / pivot_val;
-            for i in 0..COL {
+            for i in 0..col {
                 matrix[row_under_pivot][i] -= multiplier * matrix[column][i];
                 inv_matrix[row_under_pivot][i] -= multiplier * inv_matrix[column][i];
             }
@@ -55,9 +52,9 @@ pub fn invert_matrix(matrix: &Matrix4x4, ignore_4th_col: bool) -> Result<Matrix4
     }
 
     // Divide each row to turn the pivot into 1
-    for column in 0..COL {
+    for column in 0..col {
         let divisor = matrix[column][column];
-        for i in 0..COL {
+        for i in 0..col {
             matrix[column][i] /= divisor;
             inv_matrix[column][i] /= divisor;
         }
@@ -65,10 +62,10 @@ pub fn invert_matrix(matrix: &Matrix4x4, ignore_4th_col: bool) -> Result<Matrix4
     }
 
     // Backward substitution
-    for row in 0..ROW {
-        for column in (row+1)..COL {
+    for row in 0..row {
+        for column in (row+1)..col {
             let constant = matrix[row][column];
-            for i in 0..COL {
+            for i in 0..col {
                 matrix[row][i] -= matrix[column][i] * constant;
                 inv_matrix[row][i] -= inv_matrix[column][i] * constant;
             }
@@ -76,7 +73,7 @@ pub fn invert_matrix(matrix: &Matrix4x4, ignore_4th_col: bool) -> Result<Matrix4
             matrix[row][column] = 0.0;
         }
     }
-    Ok(inv_matrix)
+    Ok(())
 }
 
 
@@ -87,15 +84,36 @@ mod tests {
 
     #[test]
     fn invert_trs_matrix() {
-        let mat = [
-            [1.0, 0.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 0.0, 1.0],
-            [3.0, 10.0, 1.0]
+        let mut mat = [
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [3.0, 10.0, 1.0, 1.0]
         ];
-        let result = invert_matrix(&mat).unwrap();
-        assert_eq!(round_place(result[3][0], 2), -3.0);
-        assert_eq!(round_place(result[3][1], 2), -10.0);
-        assert_eq!(round_place(result[3][2], 2), -1.0);
+
+        invert_matrix(&mut mat, true).unwrap();
+        assert_eq!(round_place(mat[3][0], 2), -3.0);
+        assert_eq!(round_place(mat[3][1], 2), -10.0);
+        assert_eq!(round_place(mat[3][2], 2), -1.0);
+    }
+
+    #[test]
+    fn invert_whole_matrix() {
+        let mut matrix: Matrix4x4 = [
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 16.0],
+        ];
+
+        let result = [
+            [3.85953, 1.42211, -0.12309, -0.73853],
+            [-3.17385, -0.98748, 0.10014, 0.60083],
+            [-0.4242, -0.30946, 0.02434, 0.14604],
+            [0.31154, -0.01043, -0.08345, -0.0007]
+        ];
+
+        invert_matrix(&mut matrix, false).unwrap();
+        assert_eq!(matrix, result);
     }
 }
